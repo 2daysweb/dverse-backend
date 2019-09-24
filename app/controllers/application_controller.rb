af455b8ffc12dd2f3dda162a086b4b20453caba7
shmohamud <shmohamud@email.wm.edu>
2019-09-24 19:03:14 -0400

    heroku start

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,15 +14,4 @@ require 'jwt'
         JWT.decode(token, secret_key, true, {algorithm: 'HS256'})[0]
     end 
 
-    def self.verify_curr_message
-      
-    @verifier = MessageVerifier.new(secret_key, digest:'SHA256',  serializer: JSON)
-    #Make verified message
-    @signed_message = @verifier.generate(token)
-    #Consider limit exp date ---- more secure? --- & rotating--- 
-    cookies[:remember_me] = @verifier.generate([@user.id, 100.years.from_now])
-    #create verfier 
-    token = @verifier.generate(token)
-    @verifier.verify(token)
-    end
-    end 
+end 
