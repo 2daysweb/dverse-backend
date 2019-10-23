d902eefd6a26a7bbf1bc170e10676a5b548e04cc
shmohamud <shmohamud@email.wm.edu>
2019-10-23 02:22:12 -0400

    syntax

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -7,16 +7,20 @@ class Api::V1::JobsController < ApplicationController
     render json: @Jobs, status: 200
   end
 
+  def show
+    render json: @Job, status: 200
+  end
+
   def create
     @Job = Job.create(body:"default body", title: "default title", industry:"default industry", job_type:"", status:"draft")
     UserJob.create(user_id:params["user_id"], job_id:@Job.id)
         render json: @Job, status: 201
   end
 
-  def update   
-    @Job.update(body: params[:body])
-    @Job.update(title: params[:title])
-    @Job.update(status: params[:status])
+  def update
+    params.each do |k, v|  
+     @Job.update(k+":", v)   
+    end
     job_ids = User.find(params[:user_id]).job_ids
       if(!job_ids.include?(params['id'].to_i))
           user = User.find(params[:user_id])
@@ -31,13 +35,10 @@ class Api::V1::JobsController < ApplicationController
     render json: { message: "removed" }, status: :ok
   end
 
-  def show
-    render json: @Job, status: 200
-  end
 
   private
   def job_params
-    params.permit(:id, :user_id :body, :title, :industry)
+    params.permit(:id, :user_id, :body, :title, :industry)
   end
 
   def set_job
