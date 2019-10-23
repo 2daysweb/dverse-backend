26dd1252bca6bba35b6ec39348b5976790d6236e
shmohamud <shmohamud@email.wm.edu>
2019-10-23 05:04:42 -0400

    update

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -18,8 +18,11 @@ class Api::V1::JobsController < ApplicationController
   end
 
   def update
-    params.each do |k, v|  
-     @Job.update(:k, v)   
+    params.each do |k, v| 
+      puts k 
+      puts v
+      v[k] = v[k]
+     @Job.update(k, v)   
     end
     job_ids = User.find(params[:user_id]).job_ids
       if(!job_ids.include?(params['id'].to_i))
