a7d1fe59077f2f4770fbd2d0ba78e3508ec1a123
shmohamud <shmohamud@email.wm.edu>
2019-10-24 00:59:13 -0400

    Revert "routes"
    This reverts commit d445097e27c4f4feebd1a6f31725a1808debf338.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  namespace :api do 
+    namespace :v1 do 
       post '/login', to: 'auth#create' 
       get '/profile', to: 'users#show' 
       resources :users
