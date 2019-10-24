d445097e27c4f4feebd1a6f31725a1808debf338
shmohamud <shmohamud@email.wm.edu>
2019-10-23 23:56:11 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  namespace :api do 
-    namespace :v1 do 
       post '/login', to: 'auth#create' 
       get '/profile', to: 'users#show' 
       resources :users
